f0117be1159c9e256bfc86029a5f83cccc387aa7
danillobr <danillorodriguesabreu@gmail.com>
2022-09-07 19:59:25 -0300

    Atualizando done

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return response.status(400).json({ error: "User not found!" })
+    return response.status(404).json({ error: "User not found!" })
   }
 
   request.user = user;
@@ -76,22 +76,33 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { user } = request;
   const { id } = request.params;
-  
+
   const updateTodo = user.todos.find((todo) => todo.id === id);
 
-  if(!updateTodo){
+  if (!updateTodo) {
     return response.status(404).json({ error: 'Todo not found!' });
   }
 
   updateTodo.title = title;
-  updateTodo.deadline = new Date (deadline);
+  updateTodo.deadline = new Date(deadline);
 
   return response.status(200).send();
 
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const updateTodo = user.todos.find((todo) => todo.id === id);
+
+  if (!updateTodo) {
+    return response.status(404).json({ error: "Todo not found! " });
+  }
+
+  updateTodo.done = true;
+
+  return response.status(200).send();
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
